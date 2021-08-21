9ee28cc528cc87de7980d5a85ff2a854042001cf
Faiq Ali <faiqueali017@gmail.com>
2021-08-21 23:28:16 +0500

    Set custom user model

--- 03 -Setup a Database/profiles_project/settings.py
+++ 03 -Setup a Database/profiles_project/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
