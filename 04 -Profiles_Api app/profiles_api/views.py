b1d330a63c199a5a24fe64e2ace33b71e385d1a3
Faiq Ali <faiqueali017@gmail.com>
2021-08-24 12:08:55 +0500

    Added search profiles features

--- 04 -Profiles_Api app/profiles_api/views.py
+++ 04 -Profiles_Api app/profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
@@ -98,5 +99,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+
     authentication_classes = (TokenAuthentication,)
     permissions_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
