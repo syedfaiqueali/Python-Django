4f770cf098a5556f97596b2e0b5c5dd32fc87314
Faiq Ali <faiqueali017@gmail.com>
2021-08-24 18:20:26 +0500

    Create ViewSet for our profile feed item

--- 04 -Profiles_Api app/profiles_api/urls.py
+++ 04 -Profiles_Api app/profiles_api/urls.py
@@ -8,6 +8,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet) #no need for basename as 'queryset' defined in views
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
--- 04 -Profiles_Api app/profiles_api/views.py
+++ 04 -Profiles_Api app/profiles_api/views.py
@@ -112,3 +112,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
