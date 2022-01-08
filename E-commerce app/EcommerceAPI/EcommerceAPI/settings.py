697fa8e664ebe196156cdc8e0831f1dcc3164d9d
faiqueali017 <faiqueali017@gmail.com>
2022-01-08 21:28:51 +0500

    Create serializers for models

--- E-commerce app/EcommerceAPI/EcommerceAPI/settings.py
+++ E-commerce app/EcommerceAPI/EcommerceAPI/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BooksAPIApp'
+    'BooksAPIApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
