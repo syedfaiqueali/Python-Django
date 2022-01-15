3b3b5f6ebf6d0fa2bd35d21ed6bbd58bdc0910ed
faiqueali017 <faiqueali017@gmail.com>
2022-01-15 23:15:42 +0500

    Create views

--- Watchmate/watchmate/watchlist_app/views.py
+++ Watchmate/watchmate/watchlist_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from watchlist_app.models import Movie
+
 
 # Create your views here.
+def movie_list(request):
+    #1- To get all available obj from dB
+    movies = Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+        }
+
+    return JsonResponse(data)
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active
+        }
+    return JsonResponse(data)
