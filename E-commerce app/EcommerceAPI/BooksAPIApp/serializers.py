1c2a8a3b36a861a36a916560d859afdc6a72ea15
faiqueali017 <faiqueali017@gmail.com>
2022-01-15 22:41:34 +0500

    Make starter project for watchlist

--- E-commerce app/EcommerceAPI/BooksAPIApp/serializers.py
+++ E-commerce app/EcommerceAPI/BooksAPIApp/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import Category, Book, Product, User, Order
+from django.contrib.auth.models import User
+
+from .models import Category, Book, Product, Order
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -51,20 +53,6 @@ class ProductSerializer(serializers.ModelSerializer):
             'date_created'
         )
 
-
-class UserSerializer(serializers.ModelSerializer):
-    """Serializes a User object"""
-
-    class Meta:
-        model = User
-        fields = (
-            'user_id',
-            'name',
-            'email',
-            'address',
-            'number'
-        )
-
 class OrderSerializer(serializers.ModelSerializer):
     """Serializes a Order object"""
 
@@ -76,3 +64,28 @@ class OrderSerializer(serializers.ModelSerializer):
             'product',
             'date_created'
         )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializes a User object"""
+
+    class Meta:
+        model = User
+        fields = ('id','username','email')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """Serializes a User object"""
+
+    class Meta:
+        model = User
+        fields = ('id','username','email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password'],
+        )
+        return user
--- E-commerce app/EcommerceAPI/BooksAPIApp/urls.py
+++ E-commerce app/EcommerceAPI/BooksAPIApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from knox import views as knox_views
 
-from .views import ListCategory, DetailCategory, ListBook, DetailBook, ListProduct, DetailProduct, UserViewSet, OrderViewSet
+from .views import ListCategory, DetailCategory, ListBook, DetailBook, ListProduct, DetailProduct, OrderViewSet, UserViewSet, RegisterAPI, LoginAPI
 
 
 router = DefaultRouter()
@@ -17,6 +18,11 @@ urlpatterns = [
 
     path('products', ListProduct.as_view(), name='products'),
     path('products/<int:pk>/', DetailProduct.as_view(), name='singleproduct'),
+
+    path('register',RegisterAPI.as_view(), name='register'),
+    path('login', LoginAPI.as_view(), name='login'),
+    path('logout', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall', knox_views.LogoutAllView.as_view(), name='logoutall'),
 
     path('', include(router.urls)),
 ]
