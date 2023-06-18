94f28ba001a8727bd9856f27ccfd3396ee1e1e16
Faique <syed.faique@koderlabs.com>
2023-06-18 17:57:50 +0500

    Superuser support added

--- Recipe-app-api/app/core/tests/test_models.py
+++ Recipe-app-api/app/core/tests/test_models.py
@@ -30,3 +30,21 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'testpass123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """
+        Tests that creating a user without an email raises
+        a ValueError.
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'testpass123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
