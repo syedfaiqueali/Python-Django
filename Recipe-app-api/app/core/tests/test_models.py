f0e53bbfd0a85093e79f4a05bb0a68c691b415d3
Faique <syed.faique@koderlabs.com>
2023-06-18 17:49:11 +0500

    Normalize email address

--- Recipe-app-api/app/core/tests/test_models.py
+++ Recipe-app-api/app/core/tests/test_models.py
@@ -18,3 +18,15 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertEqual(user.password, password)
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@EXAMPLE.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'testpass123')
+            self.assertEqual(user.email, expected)
