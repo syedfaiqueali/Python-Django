16fd682e3226de50bae02f91a08bc5aee804ca96
Faique <faiqueali017@gmail.com>
2023-06-24 19:19:50 +0500

    Create user model tests

--- Recipe-app-api/app/app/urls.py
+++ Recipe-app-api/app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +28,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api=docs'
     ),
+    path('api/user/', include('user.urls')),
 ]
