33631bdd2ee478b14d4d268ddc017e868a56695f
Faique <faiqueali017@gmail.com>
2023-06-27 17:56:42 +0500

    Implemented recipe listing api and its test cases

---
+++ Recipe-app-api/app/recipe/serializers.py
@@ -0,0 +1,15 @@
+"""
+Serializers for the recipe API View.
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
