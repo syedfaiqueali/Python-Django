ecf49151463bcd094a38c633c185cf8573aadb0f
Faique <faiqueali017@gmail.com>
2023-06-28 20:14:21 +0500

    Update tag api using mixin.UpdateModelMixin

--- Recipe-app-api/app/recipe/tests/test_tags_api.py
+++ Recipe-app-api/app/recipe/tests/test_tags_api.py
@@ -18,6 +18,11 @@ from recipe.serializers import (
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail URL."""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a new user."""
     return get_user_model().objects.create_user(email=email,password=password)
@@ -69,3 +74,15 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag"""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+        
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
