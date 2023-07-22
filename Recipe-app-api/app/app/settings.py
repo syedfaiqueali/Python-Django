d321ad9143741b1c1a8637506866a345bee23a54
Faique <faiqueali017@gmail.com>
2023-07-22 20:14:17 +0500

    Update django settings

--- Recipe-app-api/app/app/settings.py
+++ Recipe-app-api/app/app/settings.py
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1orj1k#ycop^_4f3@c)l+-cs%crwdd(tw9iw24z$#btl*3hd15'
+# django-insecure-1orj1k#ycop^_4f3@c)l+-cs%crwdd(tw9iw24z$#btl*3hd15
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 
 # Application definition
